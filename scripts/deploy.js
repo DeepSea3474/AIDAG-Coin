5c450ec0e72dd30ffec2fef5fffacc60d52b7e78
Aydın Akyüz <seninmailin@example.com>
2025-10-19 14:44:35 +0300

    Ritüel: AIDAG-Coin doğumu

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying with:", deployer.address);
+
+  const Contract = await ethers.getContractFactory("AIDAGCoin");
+  const contract = await Contract.deploy();
+  await contract.deployed();
+
+  console.log("Deployed to:", contract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
